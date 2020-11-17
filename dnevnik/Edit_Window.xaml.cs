c5ba37956ba478de3c075db38e88f325110b5dad
misako141 <65210068+misako141@users.noreply.github.com>
2020-11-18 01:43:10 +0500

    Add files via upload

---
+++ dnevnik/Edit_Window.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace dnevnik
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            string fio = fioBOX.Text;
+            string group = groupBOX.Text;
+            string birthdate = birthdateBOX.Text;
+            string datezadaniya = datezadaniyaBOX.Text;
+            string tema = temaBOX.Text;
+
+            int ocenka = 0;
+
+            if (ocenkaBOX.Text != "")
+            {
+                ocenka = Convert.ToInt32(ocenkaBOX.Text);
+            }
+
+            string ssql = $"INSERT INTO students_table  VALUES ( '{fio}','{group}','{birthdate}')"; 
+            string ssql2 = $"INSERT INTO work_table VALUES ( '{datezadaniya}','{tema}','{ocenka}')";
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=dnevnik;Integrated Security=True"; 
+
+            SqlConnection conn = new SqlConnection(connectionString); 
+            conn.Open();
+
+            SqlCommand command = new SqlCommand(ssql, conn);
+            int number = command.ExecuteNonQuery();
+            SqlConnection conn2 = new SqlConnection(connectionString); 
+            conn2.Open();
+
+            SqlCommand command2 = new SqlCommand(ssql2, conn2);
+            int number2 = command2.ExecuteNonQuery();
+            
+            MessageBox.Show("Данные были добавлены.");
+          
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string date = dateBOX.Text;
+            string tema = temaBOX.Text;
+            string ocenka = osenkaBOX.Text;
+            
+            string ssql = $"UPDATE work_table SET tema = '{tema}', ocenka = '{ocenka}' WHERE datezadaniya = '{date}'"; //Запрос
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=dnevnik;Integrated Security=True";
+
+            SqlConnection conn = new SqlConnection(connectionString); // Подключение к БД
+            conn.Open();// Открытие Соединения
+
+            SqlCommand command = new SqlCommand(ssql, conn);// Объект вывода запросов
+            SqlDataReader reader = command.ExecuteReader(); // Выаолнение запроса вывод информации
+            reader.Close();
+            
+            MessageBox.Show("Данные были обновлены.");
+        }
+    }
+}
---
+++ dnevnik/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Console;
+using static System.Convert;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+
+namespace dnevnik
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();           
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+       
+            string table = "students_table"; 
+            string table2 = "work_table";
+            string ssql = $"SELECT * FROM {table} "; 
+            string ssql2 = $"SELECT * FROM {table2} ";
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=dnevnik;Integrated Security=True";
+            
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            SqlCommand command = new SqlCommand(ssql, conn);
+
+            SqlDataReader reader = command.ExecuteReader(); 
+            
+
+            SqlConnection conn2 = new SqlConnection(connectionString); 
+            conn2.Open();
+            SqlCommand command2 = new SqlCommand(ssql2, conn2);
+
+            SqlDataReader reader2 = command2.ExecuteReader();
+
+            fioColoumn.Items.Clear();
+            groupColoumn.Items.Clear();
+            dateColoumn.Items.Clear();
+            temaColoumn.Items.Clear();
+            ocenkaColoumn.Items.Clear();
+
+            while (reader.Read() && reader2.Read()) 
+            {
+                fioColoumn.Items.Add(reader[0]);
+                groupColoumn.Items.Add(reader[1]);
+                dateColoumn.Items.Add(reader2[1]);
+                temaColoumn.Items.Add(reader2[2]);
+                ocenkaColoumn.Items.Add(reader2[3]);
+            }
+            
+        }
+        
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window Edit_Window = new Window1();
+            Edit_Window.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            
+                string date = dateColoumn.ToString();
+                string ssql = $"DELETE FROM work_table WHERE data_zanyati = '{date}'"; 
+                string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=dnevnik;Integrated Security=True";
+                SqlConnection conn = new SqlConnection(connectionString); 
+                conn.Open();
+
+                SqlCommand command = new SqlCommand(ssql, conn);
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                dateColoumn.Items.Clear();
+                temaColoumn.Items.Clear();
+                ocenkaColoumn.Items.Clear();
+
+                while (reader.Read()) 
+                {
+                    dateColoumn.Items.Add(reader[1]);
+                    temaColoumn.Items.Add(reader[2]);
+                    ocenkaColoumn.Items.Add(reader[3]);
+                }
+
+                reader.Close();
+                MessageBox.Show("Изменения были применены.");
+            
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Window Edit_Window = new Window1();
+            Edit_Window.Show();
+        }
+    }
+
+}
